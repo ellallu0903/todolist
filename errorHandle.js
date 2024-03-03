d85070d3ce0af3108870ffe4e50e4fa5d27bab4e
luyizhen <luyizhen93@gmail.com>
2024-03-03 13:51:35 +0800

    優化 headers

--- errorHandle.js
+++ errorHandle.js
@@ -1,12 +1,6 @@
-function errorHandle(res) {
-  const headers = {
-    "Access-Control-Allow-Headers":
-      "Content-Type, Authorization, Content-Length, X-Requested-With",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "PATCH, POST, GET,OPTIONS,DELETE",
-    "Content-Type": "application/json",
-  };
+const headers = require("./headers");
 
+function errorHandle(res) {
   res.writeHead(400, headers);
   res.write(
     JSON.stringify({
